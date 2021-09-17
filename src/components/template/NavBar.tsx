06fb3618d491ddfb4a54d8fb07ebff40daf7e63e
Thales Henrique <thhenrique@uolinc.com>
2021-09-17 14:05:34 -0300

    Adicionando componente Logo

--- src/components/template/NavBar.tsx
+++ src/components/template/NavBar.tsx
@@ -1,9 +1,17 @@
 import { IconConfig, IconHome, IconNotifier } from "../icons/icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function NavBar() {
     return (
         <aside>
+            <div className={`
+                flex flex-col items-center justify-center
+                bg-gradient-to-r from-indigo-500 to-purple-800
+                h-20 w-20
+            `}>
+                <Logo />
+            </div>
             <ul>
                 <MenuItem icon={IconHome} url="/" text="Início" />
                 <MenuItem icon={IconConfig} url="/config" text="Ajustes" />
